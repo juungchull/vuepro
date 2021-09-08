9ec455179fb4bbb024c739fc52108619f104530f
juungchull <chul6147@gamil.com>
2021-09-08 16:02:27 +0900

    menu

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,10 @@
     <v-app-bar-nav-icon @click="drawer= !drawer"/>
       <site-title :title="title"></site-title>
     <v-spacer/>
-    <v-btn icon to="/about">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-    <v-btn icon to="/">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
-      wefwef
+
+      <site-menu></site-menu>
     </v-navigation-drawer>
     <v-content>
       <router-view/>
@@ -29,9 +24,10 @@
 import Vue from 'vue'
 import SiteTitle from '@/views/site/title.vue'
 import SiteFooter from '@/views/site/footer.vue'
+import SiteMenu from '@/views/site/menu.vue'
 
 export default Vue.extend({
-  components: { SiteTitle, SiteFooter },
+  components: { SiteTitle, SiteFooter, SiteMenu },
   name: 'App',
 
   data: () => ({
--- src/views/site/menu.vue
+++ src/views/site/menu.vue
@@ -1,9 +1,30 @@
 <template>
-  <v-list-item v-for="item in items" :key="item.title" :to="item.to">
-    <v-list-item-content>
-      {{item.title}}
-    </v-list-item-content>
-  </v-list-item>
+<div>
+<v-list>
+      <v-list-group
+        v-for="(item, i) in items"
+        :key="i"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item
+          v-for="subItem in item.subItems"
+          :key="subItem.title"
+          :to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+</div>
 </template>
 <script>
 export default {
@@ -13,8 +34,30 @@ export default {
       items: [
         {
           title: 'home',
-          to: '/'
+          icon: 'mdi-home',
+          subItems: [
+            {
+              title: 'Dashboard',
+              to: '/'
+            },
+            {
+              title: 'about',
+              to: '/about'
+            }
+          ]
+        },
+        {
+          title: 'about',
+          active: false,
+          icon: 'mdi-account',
+          subItems: [
+            {
+              title: 'xxx',
+              to: 'xxx'
+            }
+          ]
         }
+
       ]
     }
   }
